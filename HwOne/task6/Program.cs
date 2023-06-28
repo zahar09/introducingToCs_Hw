ab50dbc688df3bc65ea2d7243b588d6ce40bec88
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-28 22:48:16 +0300

    Решил задачу 6 и исправил задачу 2

---
+++ HwOne/task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ввидите число: ");
+int a  = int.Parse(Console.ReadLine());
+if(a % 2 == 0){
+    Console.WriteLine($"{a} -> четное");
+}
+else{
+    Console.WriteLine($"{a} -> нечетное");
+}
