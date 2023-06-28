bed4f180218adac26f7534fc362a375f8c79aff7
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-28 22:32:18 +0300

    Решил задачу 2

---
+++ HwOne/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Ввидите число a: ");
+int a  = int.Parse(Console.ReadLine());
+Console.Write("Ввидите число b: ");
+int b  = int.Parse(Console.ReadLine());
+
+if(a > b){
+    Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+}
+else if(b > a){
+    Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+}
+else{
+    Console.WriteLine($"a = {a}; b = {b} -> числа равны");
+}
