c0d1719b873d03502fc1f89e82fde456b41f1867
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-28 22:43:35 +0300

    Решил задачу 4

---
+++ HwOne/task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Ввидите число a: ");
+int a  = int.Parse(Console.ReadLine());
+Console.Write("Ввидите число b: ");
+int b  = int.Parse(Console.ReadLine());
+Console.Write("Ввидите число c: ");
+int c  = int.Parse(Console.ReadLine());
+
+if(a > b){
+    if(a > c){
+        Console.WriteLine($"{a}, {b}, {c} -> {a}");
+    }
+    else{
+        Console.WriteLine($"{a}, {b}, {c} -> {c}");
+    }
+}
+else{
+    if(b > c){
+        Console.WriteLine($"{a}, {b}, {c} -> {b}");
+    }
+    else{
+        Console.WriteLine($"{a}, {b}, {c} -> {c}");
+    }
+}
