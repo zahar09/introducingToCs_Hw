844b914a465409c5844edfa9e86f70d384f0afc8
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-06-28 22:53:24 +0300

    Решил задачу 8

---
+++ HwOne/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Ввидите число: ");
+int a  = int.Parse(Console.ReadLine());
+
+Console.Write($"{a}-> ");
+
+for(int i = 2;i <= a;i+=2){
+    if(i+2 > a){
+        Console.Write($"{i}");
+    }
+    else{
+        Console.Write($"{i}, ");
+    }
+    
+}
