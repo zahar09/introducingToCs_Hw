3b49af77d2de7d8e860e1fb0bbde22e5ea8f5538
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-02 21:06:35 +0300

    Решил задачу 15

---
+++ HwTwo/Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day > 7 || day < 1){
+    Console.WriteLine("Токого дня недели нет) ");
+}
+else if(day == 7 || day == 6){
+    Console.WriteLine("Это выходной :)");
+}
+else{
+    Console.WriteLine("Это рабочий день недели :(");
+}
