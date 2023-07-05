fb7988ffc16ee6dda87a5088c68fa5700498768e
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-05 22:48:10 +0300

    Решил задачу 23

---
+++ HwThree/Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+PrintCubeTable(num);
+
+
+void PrintCubeTable(int number){
+    Console.Write($"{number} -> ");
+    for(int i = 1;i < number;i++){
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write($"{number * number * number}");
+}
