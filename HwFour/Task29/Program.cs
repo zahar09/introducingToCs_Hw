5d5fd16981b06346cc6c89e0924d6f2e2136ce0f
Zakharov Ilia <ilya.zaharov.2009@mail.ru>
2023-07-08 16:06:10 +0300

    Решил задачу 29

---
+++ HwFour/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int sizeOfArr = 8;
+int[] array = GetArr(sizeOfArr);
+PrintArr(array);
+
+int[] GetArr(int size){
+    int[] arr = new int[size];
+    for(int i = 0;i < size;i++){
+        arr[i] = new Random().Next(100);
+
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr){
+    int arr_size = arr.Length;
+    Console.Write("[");
+    for(int i = 0;i <  arr_size - 1;i++){
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr_size - 1]}]");
+}
