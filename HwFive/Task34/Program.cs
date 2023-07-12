ce052ea45f5751ce72e96ab97a6b471f0b076112
Ilya Zakharov <ilya.zaharov.2009@mail.ru>
2023-07-12 14:50:02 +0300

    Решил задачу 34

---
+++ HwFive/Task34/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+int[] arr = GetArr();
+int ans = CheckArr(arr);
+Console.Write($"{String.Join(", ",arr)} -> {ans}");
+
+int[] GetArr(){
+    int n = new Random().Next(2,10);
+    int[] arr = new int[n];
+    for(int i = 0; i < n;i++){
+        arr[i] = new Random().Next(100,1000);
+    }
+    return arr;
+}
+
+int CheckArr(int[] arr){
+    int numbers = 0;
+    for(int i = 0;i < arr.Length;i++){
+        if(arr[i] % 2  == 0){
+            numbers++;
+        }
+    }
+    return numbers;
+}
