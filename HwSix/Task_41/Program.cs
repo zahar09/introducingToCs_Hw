ec3e1dd945ef77651c18558f634787e1fcabd093
Ilya Zakharov <ilya.zaharov.2009@mail.ru>
2023-07-16 17:43:49 +0300

    Решил задачу 41

---
+++ HwSix/Task_41/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Введите числа через пробел: ");
+string numbers_string = Console.ReadLine();
+
+int[] arr = CreatArrFromString(numbers_string);
+int quantity = CountNumbers(arr);
+
+Console.WriteLine(quantity);
+
+
+int[] CreatArrFromString(string string_arr){
+    string[] arr = string_arr.Split(" ");
+    int[] numbers = new int[arr.Length];
+    for(int i = 0;i < numbers.Length;i++){
+        numbers[i] = int.Parse(arr[i]);
+    }
+    return numbers;
+}
+
+int CountNumbers(int[] array){
+    int quantity = 0;
+    for(int i = 0;i < array.Length;i++){
+        if(array[i] > 0){
+            quantity++;
+        }
+    }
+    return quantity;
+}
+
+
+
+
+
